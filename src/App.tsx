6aaf3608cbad4fbf858bab3c1ccfa0b3242f541e
João lucas <joaolucasdesouzapaz1@gmail.com>
2022-07-04 20:41:13 -0300

    Atividade 04/07/2022

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Login from './pages/login/Login';
 import Home from './pages/home/Home';
 import './App.css';
 import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
+import ListaTema from './components/temas/listatema/listatema';
+import ListaPostagem from './components/postagens/listapostagem/listapostagem';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
             <Route path="/login" element={<Login />} /> 
             <Route path="/home" element={<Home />} />
             <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+            <Route path="/temas" element={<ListaTema />} />
+            <Route path="/posts" element={<ListaPostagem />} />
           </Routes>
         </div>
       <Footer />
